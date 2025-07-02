56706dfe132f23bab8bfe33a53c92e73cc51f5cf
saideep1629 <saideepmanve3@gmail.com>
2025-07-02 17:04:40 +0530

    routing done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
-const App = () =>{
-    return(
-      <>
-      <h1 className="text-blue-600">hello world</h1>
-      </>
-    )
-}
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
