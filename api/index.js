6b63ea5d2445af1437291ea21432c960175f39e2
saideep1629 <saideepmanve3@gmail.com>
2025-07-03 19:26:37 +0530

    mongodb connection done

--- api/index.js
+++ api/index.js
@@ -1,7 +1,30 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+dotenv.config({
+  path: "./.env",
 });
+
+const connectDB = async () => {
+  try {
+    const connectInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${process.env.DB_NAME}`
+    );
+    console.log(`connected successfully and host is ${connectInstance.connection.host}`);
+  } catch (error) {
+    console.log("MONGODB connection failed", error);
+  }
+};
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("server connection failed", err);
+  });
